5881f19b0affca8a20c58ab585682d81153d6b6c
ivjose <ivjosesantos@gmail.com>
2021-07-15 17:07:40 +0800

    fixed husky and lingstage config

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const data = 'sample'
-  const data = ''
   return (
     <div className="App">
       <header className="App-header">
